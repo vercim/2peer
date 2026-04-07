7bd2c35403456b27fd2206835437ba0271fbb76e
kinovw <67692844+kinovw@users.noreply.github.com>
2026-04-07 18:20:36 +0300

    v4 fix

--- src/renderer.js
+++ src/renderer.js
@@ -40,6 +40,14 @@ let reconnectTimer   = null;
 let isBroadcasting   = false;
 let isStoppingBroadcast = false;
 
+const rtcConfig = {
+  iceServers: [
+    { urls: ['stun:stun.l.google.com:19302', 'stun:stun1.l.google.com:19302'] },
+    { urls: 'turn:openrelay.metered.ca:80',  username: 'openrelayproject', credential: 'openrelayproject' },
+    { urls: 'turn:openrelay.metered.ca:443', username: 'openrelayproject', credential: 'openrelayproject' }
+  ]
+};
+
 function setStatus(msg, isErr = false) {
   const entry = document.createElement('div');
   entry.className = 'entry' + (isErr ? ' error' : '');
